bbbdea847efa4aa684ef147027dd3ae3a4c657c7
Veena Kumari <matajee1012@gmail.com>
2019-09-28 15:11:58 +0530

    added Book.java

--- Book.java
+++ Book.java
@@ -1,6 +1,23 @@
 class Book{
 private int bookId;
 private String name;
-
+public void setbookId()
+{
+	
+this.id=bookId
+}
+public int getBookId(int bookId)
+{
+return bookId;
+	
+}
+public void setName()
+{
+this.name=name;
+}
+public String getName(String name)
+{
+	return name;
+}
 
 }
