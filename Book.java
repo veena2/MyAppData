b1b1eabeba0653d91c73fee43f831f25cfb9a943
Veena Kumari <matajee1012@gmail.com>
2019-09-28 15:16:04 +0530

    added one more line

--- Book.java
+++ Book.java
@@ -3,13 +3,12 @@ private int bookId;
 private String name;
 public void setbookId()
 {
-	
 this.id=bookId
+System.out.println("Book Id is Set");
 }
 public int getBookId(int bookId)
 {
 return bookId;
-	
 }
 public void setName()
 {
